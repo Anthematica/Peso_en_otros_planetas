829308a4a0322f4bf40f5074e37525320beef38e
Anthematica <36492022+Anthematica@users.noreply.github.com>
2021-02-15 21:10:54 -0600

    Probando las conexiones entre el archivo html y el javascript

--- programacion.js
+++ programacion.js
@@ -1,9 +1,32 @@
-var usuario = prompt("Cual es tu peso en la Tierra?");
-		var planeta = prompt("De que planeta deseas calcular tu peso? \n 1. Marte 2. Jupiter");
 
-		var masa_Tierra = parseFloat(usuario); 
+    //Constantes
+    var g_Tierra = 9.8;
+    var g_Jupiter = 24.6;
+    var g_Marte = 3.7;
+
+    //elementos de html
+    var masa_Tierra = document.getElementById("peso");
+    var button = document.getElementById("bottom");
+
+    button.addEventListener("click", peso_marte);
+
+    function peso_marte(){
+        var peso_Tierra = masa_Tierra.value;
+
+            peso = peso_Tierra*g_Marte/g_Tierra;
+			peso_final = peso.toFixed(2); 
+            
+           //Forma de enviar un resultado a una caja de texto 
+           document.getElementById("impresion").value = peso_final;
+
+    }
+
+   
+
+
 
 		//Gravedades
+        /*
 		var g_Tierra = 9.8;
 		var g_Jupiter = 24.6;
 		var g_Marte = 3.7;
@@ -29,3 +52,5 @@ var usuario = prompt("Cual es tu peso en la Tierra?");
 		}
 		//Impresion
 		document.write("Tu peso en " +name+" es " + peso_final + " Kg");
+
+        */
